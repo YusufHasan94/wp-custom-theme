ac00c01e610e12260ba19d084e7b594cceb25223
yusuf444 <yusuf.hasan@vivasoftltd.com>
2025-01-22 00:44:41 +0600

    adding content

--- index.php
+++ index.php
@@ -28,7 +28,15 @@
             </div>
         </div>
     </div>
-
+    <section id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php the_content();?>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     <?php wp_footer()?>    
