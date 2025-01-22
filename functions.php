3f2991d0cfd5fed076d65233517d8e1fc731f3be
yusuf444 <yusuf.hasan@vivasoftltd.com>
2025-01-22 22:13:20 +0600

    add menu items description

--- functions.php
+++ functions.php
@@ -62,3 +62,14 @@ function yusuf_customizer_register($wp_customize){
 add_action('customize_register', 'yusuf_customizer_register');
 
 register_nav_menu( 'main_menu', __('Main Menu', 'customrajtheme'));
+
+//walker nav
+
+function yusuf_walker_nav($item_output, $item, $args){
+    if(!$itme->description){
+        $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">'.$item->description.'</span>' . $args->link_after . '</a>', $item_output);
+    }
+    return $item_output;
+}
+
+add_filter( 'walker_nav_menu_start_el', 'yusuf_walker_nav', 10, 3);
